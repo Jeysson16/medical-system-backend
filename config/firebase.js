55a0bd36c1cab7c701d19b0e8d005971b705bb53
Jeysson Sánchez <jeysson_s.r@hotmail.com>
2024-11-26 10:33:39 -0500

    env con sdk prueba

--- config/firebase.js
+++ config/firebase.js
@@ -2,15 +2,7 @@ const admin = require("firebase-admin");
 
 try {
     console.log("Initializing Firebase...");
-    const serviceAccount = {
-        apiKey: "AIzaSyD-uzdVngolTs-quZgL_kY-Mb4uoyFBtOA",
-        authDomain: "capsule-852e9.firebaseapp.com",
-        projectId: "capsule-852e9",
-        storageBucket: "capsule-852e9.appspot.com",
-        messagingSenderId: "547809404822",
-        appId: "1:547809404822:web:c7778452587072c5765fb7",
-        measurementId: "G-GNXJVDX3RY"
-    };
+    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
     console.log("Service Account Loaded:", serviceAccount);
 
     admin.initializeApp({
