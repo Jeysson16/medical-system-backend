ca5840e45506cf14ec381c30c60e081a028b7b9b
Jeysson Sánchez <jeysson_s.r@hotmail.com>
2024-11-26 10:25:00 -0500

    env con sdk prueba

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,19 @@
 const admin = require("firebase-admin");
 
-// Parsear las credenciales desde una variable de entorno
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
+try {
+    console.log("Initializing Firebase...");
+    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
+    console.log("Service Account Loaded:", serviceAccount);
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "capsule-852e9.appspot.com"
-});
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket: "your-project-id.appspot.com"
+    });
+
+    console.log("Firebase Initialized");
+} catch (error) {
+    console.error("Firebase Initialization Error:", error);
+}
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
